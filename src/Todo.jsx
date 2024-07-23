5d3b2d0f23c832982bc71df3360e19e0d44169ce
goro-ne <hayao56@gmail.com>
2024-07-23 09:31:05 +0900

    TODO追加

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react';
 import './styles.css'
 
 export const Todo = () => {
+  // ステート定義
+  const [todoText, setTodoText]　= useState("");
   const [incompleteTodos, setIncompleteTodos]
     = useState(["TODOです１", "TODOです２"]);
   const [completeTodos, setCompleteTodos]
     = useState(["TODOでした１", "TODOでした２"]);
+  // テキスト入力
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  // 追加ボタン
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const nreTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(nreTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder='TODOを入力'></input>
-        <button>追加</button>
+        <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}></input>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
