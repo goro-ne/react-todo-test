71bdc89de3fb9a4fe346fd6162ba4bcac97b04cc
goro-ne <hayao56@gmail.com>
2024-07-23 11:59:23 +0900

    TODOが最大値を超えた場合の処理を追加

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,9 +8,9 @@ export const Todo = () => {
   // ステート定義
   const [todoText, setTodoText]　= useState("");
   const [incompleteTodos, setIncompleteTodos]
-    = useState(["TODOです１", "TODOです２"]);
+    = useState([]);
   const [completeTodos, setCompleteTodos]
-    = useState(["TODOでした１", "TODOでした２"]);
+    = useState([]);
   // テキスト入力
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   // 追加ボタン
@@ -42,13 +42,21 @@ export const Todo = () => {
     newCompleteTodos.splice(index, 1);
     setCompleteTodos(newCompleteTodos);
   };
+  // TODOが最大値を超えたか判定
+  const isMaxLimitIncompleteTodos = incompleteTodos.length >= 5;
   return (
     <>
       <InputTodo
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={isMaxLimitIncompleteTodos}
       />
+      {isMaxLimitIncompleteTodos && (
+        <p　style={{color: "red"}}>
+          登録できるTODOは５個までです。TODOを消化してください。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
